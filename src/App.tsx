9fd91142e3d1533610ac1ce13170bc54c12b2870
ShaneEDouglas <112830171+ShaneEDouglas@users.noreply.github.com>
2025-05-26 13:05:12 -0400

    Inital commit with folders and files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/index.css'; 
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Invenflow App
+      </h1>
+      <p className="text-3xl underline">maybe</p>
+      
+    </>
+  )
+}
+
+export default App
